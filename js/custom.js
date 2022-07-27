60133c51ac4260288657741501c11b9cbc99a4b6
jahidulkanchan <103818133+jahidulkanchan@users.noreply.github.com>
2022-07-27 11:22:17 +0600

    new add

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,44 @@
 window.addEventListener('scroll', function(){
     var header = document.getElementById('header');
     header.classList.toggle('sticky', window.scrollY > 300)
-})
+});
+//Smooth-scroll js
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");  
+        $('.scroll-to-section a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+        var target = this.hash,
+        menu = target;
+        var target = $(this.hash);
+        $('html, body').stop().animate({
+            scrollTop: (target.offset().top) + 1
+        }, 100, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+  });
+  
+  function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#main-menu li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#main-menu li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+  }
+
 //nav toggle
 var navToggler = document.getElementById("nav-toggle");
 var mainMenu = document.getElementById('main-menu');
@@ -14,14 +51,16 @@ navToggler.onclick = function () {
   mainMenu.classList.toggle("active");
 };
 document.onclick = function (e) {
-  if (e.target.id !== "nav-toggle" && e.target.id !== "main-menu" ) {
+  if (e.target.id !== "nav-toggle" && e.target.id !== "menu" ) {
     navToggler.classList.remove("active");
     mainMenu.classList.remove("active");
   }
 };
 
-
-
+//active menu
+// $(document).on('click', '#main-menu ul li', function(){
+//     $(this).addClass('active').siblings().removeClass('active');
+// })
 
 //dropdown-btn
 // $('#dropdown').on('click',function(){
@@ -53,47 +92,9 @@ window.addEventListener("scroll" , function(){
   topScroll.classList.toggle("visible", window.scrollY > 400 );
 })
 
-//active menu
-// $(document).on('click', 'ul.menu li a', function(){
-//   $(this).addClass('active').siblings().removeClass('active');
-// })
 
-//Smooth-scroll js
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-  $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");  
-      $('.scroll-to-section a').each(function () {
-          $(this).removeClass('active');
-      })
-      $(this).addClass('active');
-      var target = this.hash,
-      menu = target;
-      var target = $(this.hash);
-      $('html, body').stop().animate({
-          scrollTop: (target.offset().top) + 1
-      }, 100, 'swing', function () {
-          window.location.hash = target;
-          $(document).on("scroll", onScroll);
-      });
-  });
-});
 
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.menu a').each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.menu li a').removeClass("active");
-          currLink.addClass("active");
-      }
-      else{
-          currLink.removeClass("active");
-      }
-  });
-}
+
 //owl carousel
 $('.slide1').owlCarousel({
   loop: true,
@@ -145,9 +146,8 @@ function hideProgress(){
 });
 */
 //counter
-$('.counter-up').counterUp({
+$('.counter').counterUp({
     delay: 10,
     time: 1000
 });
-
 
